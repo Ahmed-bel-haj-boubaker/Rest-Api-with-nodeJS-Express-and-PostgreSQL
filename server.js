6912b00e9812c8678b6d717ce237c3c96750c014
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-03-23 02:31:55 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config({ path: ".env" });
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // for parsing JSON responses ( extract the string value from the json to javascript object)
+app.use(morgan("dev")); // morgan is used for logging HTTP requests like this GET / 200 3.077 ms - 18 ( morgan is a middleware module)
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT} `);
+});
